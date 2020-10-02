1c87b071411a0ac5b3b5b7574fc45ced92610dca
huang <huang@mail.chungyo.net>
2020-10-02 20:22:08 +0800

    k8swatch

---
+++ k8swatch/image.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+
+VER=$2
+if [ "$2" == '' ]; then
+  VER="v1.0.0"
+fi
+
+IMAGE_NAME="myk8swatch:$VER"
+echo $IMAGE_NAME
+case "$1" in
+    build)
+        docker build -t $IMAGE_NAME  -f Dockerfile .
+        ;;
+
+    push)
+        docker push $IMAGE_NAME
+        ;;
+    *)
+        echo $"Usage: $0 {build|push}"
+        exit 1
+esac
